67ad1d9706da535e8454634527935a287bb8d155
unknown <squier_84@mail.ru>
2016-08-28 02:40:30 +0300

    new file

---
+++ js/main3 .js
@@ -0,0 +1,35 @@
+// sayHello1();
+// function sayHello1() {
+//
+// }
+// sayHello1();
+
+
+// var sayHello2 = function() {
+//   /////
+// };
+// sayHello2();
+
+// a = 1;
+
+// function testMe() {
+//   'use strict';
+//   // console.log(a);
+//   var a = 10;
+
+//   function sayHi() {
+//     console.log(a);
+//   }
+//   sayHi();
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// testMe();
+
+
+function showPopup(msg, color) {
+  // console.log(arguments);
+  msg = msg || 'hello, user!';
+
